bfa2d5766f8b18e58a2ddfa55e324b34db12639e
insomnight <qifengchen12@126.com>
2023-11-30 11:29:39 +0800

    算法4练习1.1.28的java实现

---
+++ Algorithms_4th_Edition/java/src/1/1/Test1_1_28.java
@@ -0,0 +1,56 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
+public class Test1_1_28 {
+    public static int rank(int key, int[] a)
+    {  // 数组必须是有序的
+        int lo  = 0;
+        int hi = a.length -1;
+        while (lo <= hi)
+        {  // 被查找的键要么不存在，要么必然存在于a[lo..hi]之中
+            int mid = lo + (hi - lo) / 2;
+            if       (key < a[mid]) hi = mid -1;
+            else if (key > a[mid]) lo = mid + 1;
+            else                     return mid;
+        }
+        return -1;
+    }
+
+    public static int[] rmduplicate(int[] a)
+    {
+        if(a.length == 0 || a.length == 1)
+            return a;
+
+        int old = 0;
+        for(int i = 1; i < a.length; i++)
+        {
+            if(a[old] != a[i])
+                old++;
+            a[old] = a[i];
+        }
+        old++;
+        int[] na = new int[old];
+        System.arraycopy(a, 0, na, 0, old);
+        return na;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] whitelist = In.readInts(args[0]);
+        Arrays.sort(whitelist);
+        char op = args[1].charAt(0);
+        while (! StdIn.isEmpty())
+        {  // 读取键值，如果不存在于白名单中则将其打印
+            int key = StdIn.readInt();
+            if(op == '-') {
+                if (rank(key, whitelist) >= 0)
+                    StdOut.println(key);
+            } else
+            if (rank(key, whitelist) >= 0)
+                StdOut.println(key);
+        }
+    }
+}
