cff6b4619d571a08b75396a7d28f72bff2271b31
insomnight <qifengchen12@126.com>
2023-12-06 16:35:02 +0800

    算法4 1.2.2.3 counter实现添加测试用例

--- Algorithms_4th_Edition/java/src/1/2/book/Counter.java
+++ Algorithms_4th_Edition/java/src/1/2/book/Counter.java
@@ -1,30 +1,41 @@
 public class Counter {
 
-    private String id;
-    private int data;
+    private final String name;
+    private int count;
 
     /**
      * 创建一个名为id的计数器
      * @param id 计数的名
      */
     public Counter(String id) {
-        this.id = id;
-        this.data = 0;
+        this.name = id;
     }
 
     /**
      * 将计数器的值加一
      */
     public void increment(){
-        this.data++;
+        this.count++;
     }
 
     public int tally(){
-        return this.data;
+        return this.count;
     }
 
     @Override
     public String toString() {
-        return data + " " + id;
+        return count + " " + name;
+    }
+
+    public static void main(String[] args) {
+        Counter heads = new Counter("heads");
+        Counter tails = new Counter("tails");
+
+        heads.increment();
+        heads.increment();
+        tails.increment();
+
+        System.out.println(heads + " " + tails);
+        System.out.println(heads.tally() + tails.tally());
     }
 }
