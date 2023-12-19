1e54f24214dd8d07544c2cac1e2a97c51f2f67d3
insomnight <qifengchen12@126.com>
2023-12-19 08:07:10 +0800

    练习1.3.10的java实现

--- Algorithms_4th_Edition/java/src/1/3/InfixToPostfix.java
+++ Algorithms_4th_Edition/java/src/1/3/InfixToPostfix.java
@@ -1,4 +1,5 @@
 import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdIn;
 
 public class InfixToPostfix {
     public static int precede(char op)
@@ -14,9 +15,9 @@ public class InfixToPostfix {
     {
         StringBuilder c = new StringBuilder();
         Stack<Character> stack = new Stack<>();
-        int j = 0;
         char ch;
         char[] e = ex.toCharArray();
+        stack.push('@');
         for (int i = 0; i < e.length; i++) {
             ch = e[i];
             if(ch == '(')
@@ -29,8 +30,31 @@ public class InfixToPostfix {
                 while(precede(stack.peek()) >= precede(ch))
                     c.append(stack.pop());
                 stack.push(ch);
+            } else {
+                while(ch >= '0' && ch <= '9')
+                {
+                    c.append(ch);
+                    i++;
+                    if(i >= e.length)
+                        break;
+                    ch = e[i];
+                }
+                i--;
+                c.append(' ');
             }
         }
+        while(stack.peek()!='@'){
+            c.append(stack.pop());
+        }
         return c.toString();
+    }
+
+    public static void main(String[] args) {
+        while(!StdIn.isEmpty())
+        {
+            String ex = StdIn.readLine();
+            String s = RPExpression(ex);
+            System.out.println(s);
+        }
     }
 }
